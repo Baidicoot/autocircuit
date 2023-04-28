0c28ac9a90b31a5e037d4d4df6d83110da32698e
Aidan Ewart <aidanprattewart@gmail.com>
2023-04-28 13:14:32 +0100

    dh

--- autocircuit/__init__.py
+++ autocircuit/__init__.py
@@ -1,13 +1,19 @@
 import transformer_lens as lens
-from typing import Dict, Tuple, List, Str, Literal, Callable
+from typing import Dict, Tuple, List, Literal, Callable, Any
 import torch
 from typeguard import typechecked
 
+# thing representing some (contiguous) selection of activations in a network
+class PartialHook:
+    def __init__(self, hook_point: str, hook_read: Callable[torch.Tensor, torch.Tensor], hook_repl: Callable[[torch.Tensor, torch.Tensor], torch.Tensor]):
+        self.hook_point = hook_point
+        self.hook_read = hook_read
+        self.hook_repl = hook_repl
+
 # a ComputeGraph is a DAG that represents the causal relationship between hooks/nodes
 # the path tracing algorithm outputs a simpler graph that is a subset of the input graph
-
 class ComputeGraph:
-    def __init__(self, nodes: Dict[str, lens.HookPoint], edges: Tuple[str, str], root: str):
+    def __init__(self, nodes: Dict[str, PartialHook], edges: Tuple[str, str], root: str):
         self.nodes = nodes
         self.edges = edges
         self.root = root
@@ -17,21 +23,19 @@ class ComputeGraph:
     
     def no_cycles(self):
         # cycle detection algorithm
-        visited = set()
-        stack = set()
+        stack = []
         def dfs(node):
-            visited.add(node)
-            stack.add(node)
+            stack.append(node)
             for edge in self.edges:
                 if edge[0] == node:
+                    if edge[1] in stack:
+                        raise ValueError(f"Cycle detected with node {edge[1]}", stack)
                     dfs(edge[1])
-                elif edge[1] in stack:
-                    raise ValueError(f"Cycle detected with node {edge[1]}")
-            stack.remove(node)
+            stack.pop()
         dfs(self.root)
     
     def all_reachable(self):
-        # all reachable from root
+        # check all reachable from root
         visited = set()
         def dfs(node):
             visited.add(node)
@@ -42,10 +46,10 @@ class ComputeGraph:
         assert len(visited) == len(self.nodes), "Not all nodes are reachable from root"
     
     def get_parents(self, node):
-        return [edge[0] for edge in self.edges if edge[1] == node]
+        return [edge[1] for edge in self.edges if edge[0] == node]
 
     @classmethod
-    def merge_many(cls, graphs: List[ComputeGraph], root: Tuple[str, lens.HookPoint]):
+    def merge_many(cls, graphs, root):
         nodes = {root[0]: root[1]}
         edges = [(graph.root, root[0]) for graph in graphs]
         roots = [graph.root for graph in graphs]
@@ -54,7 +58,8 @@ class ComputeGraph:
             edges.extend(graph.edges)
         return cls(nodes, edges, root[0])
 
-def path_tracing(net: Callable[[List[Tuple[Callable, lens.HookPoint]], torch.Tensor], torch.Tensor], graph: ComputeGraph, node: str, prompts: torch.Tensor, counterfactual_activations: Dict[str, torch.Tensor], loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor], threshold: float, ablation_mode: Literal["zero", "sample", "mean"]) -> ComputeGraph:
+# model needs to have run_with_hooks method
+def path_tracing(model, graph: ComputeGraph, node: str, prompts: torch.Tensor, counterfactual_activations: Dict[str, torch.Tensor], diff_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor], threshold: float, ablation_mode: Literal["zero", "sample", "mean"]) -> ComputeGraph:
     # recursively find all nodes that affect the output of `node`
     parent_nodes = graph.get_parents(node)
     if len(parent_nodes) == 0:
@@ -63,17 +68,26 @@ def path_tracing(net: Callable[[List[Tuple[Callable, lens.HookPoint]], torch.Ten
     important = []
 
     # TODO: probably compress this iteration into a single forward pass
-    for parent in parent_nodes:
+    for parent_n in parent_nodes:
+        parent = graph.nodes[parent_n]
+        parent_cf = parent.hook_read(counterfactual_activations[graph.nodes[parent].hook_point])
         # rerun the network with the parent node ablated
         def ablate_node(hook_tensor, hook_point):
+            replacement = None
             if ablation_mode == "zero":
-                return torch.zeros_like(hook_tensor)
+                replacement = torch.zeros_like(parent_cf)
             elif ablation_mode == "sample":
-                return counterfactual_activations[parent][random.randint(0, counterfactual_activations.size(0))].repeat(hook_tensor.size(0))
+                replacement = parent_cf[random.randint(0, parent_cf.size(0))].repeat(parent_cf.size(0))
             elif ablation_mode == "mean":
-                return counterfactual_activations[parent].mean(dim=0).repeat(hook_tensor.size(0))
-        
-        loss = loss_fn(net([(ablate_node, graph.nodes[parent])], prompts), net([], prompts)).item()
+                replacement = parent_cf.mean(dim=0).repeat(parent_cf.size(0))
+            
+            if replacement is None:
+                raise ValueError("Invalid ablation mode")
+            
+            return parent.hook_repl(hook_tensor, replacement)
+
+        diff = diff_fn(model.run_with_hooks(prompts, fwd_hooks=[(parent.hook_point, ablate_node)]), model(prompts)).item()
+        print(f"ablated {parent_n}")
         if loss > threshold:
             important.append(parent)
     
